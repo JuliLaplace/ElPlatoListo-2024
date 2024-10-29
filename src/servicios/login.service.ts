ac5985bfc1ae06ab4f1d1d87791607a8b97f2dc6
JuliLaplace <juli.laplace@gmail.com>
2024-10-29 18:13:56 -0300

    [SERVICIOS] Agregado servicio login, servicio de sesion y servicio de validacion de formularios

---
+++ src/servicios/login.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Auth, signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+
+export interface LoginError {
+  errorFlag: boolean,
+  errorMsj:string;
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private auth: Auth, private router: Router) { }
+
+
+
+  async login (email: string, password: string): Promise <LoginError>{
+
+    let error : LoginError = {
+      errorFlag: false,
+      errorMsj :""
+    }
+
+    return signInWithEmailAndPassword(this.auth, email, password)
+    .then((resultado)=>
+    {
+      error.errorMsj=""
+      error.errorFlag=true;
+      return error;
+    })
+    .catch((e)=>
+    {
+      error.errorFlag = true;
+      switch(e.code){
+        default:
+          error.errorMsj = "Usuario o contraseña incorrecta. Reintente."
+          break;
+      }
+      return error;
+    })
+  }
+
+  async logout(): Promise<void> {
+    console.log(this.auth.currentUser?.email)
+    return signOut(this.auth)
+      .then(() => {
+        this.router.navigate(['/home']);
+      });
+  }
+}
---
+++ src/servicios/sesion.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Auth, onAuthStateChanged, User } from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SesionService {
+  
+  private usuarioActual : User | null = null;  //guardo el usuario
+
+  constructor(private auth: Auth) {
+    onAuthStateChanged(auth, (usuario)=>{
+      this.usuarioActual = usuario;  //es como un listener para escuchar el estado del usuario (si sigue logueado o no)
+    });
+   }
+
+   getUsuario(): string{
+    return this.usuarioActual?.email ? this.usuarioActual?.email : "";
+  }
+
+  sesionActiva(): boolean{
+    return this.usuarioActual !=null;
+  }
+
+}
