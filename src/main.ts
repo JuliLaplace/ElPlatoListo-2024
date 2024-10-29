37a368357ac46f570cb623e6256146fcca8df0c4
JuliLaplace <juli.laplace@gmail.com>
2024-10-29 17:08:46 -0300

    [FIREBASE] Creación de proyecto firebase - Vinculación de servicios (Cloud Firestore - Authentication) en proyecto

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,17 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withPreloading(PreloadAllModules)), 
+    provideFirebaseApp(() => initializeApp({"projectId":"el-plato-listo","appId":"1:672867578350:web:792bc52f8bd8002c619eff","storageBucket":"el-plato-listo.appspot.com","apiKey":"AIzaSyAWtH8J6rQU1LZ-8ixY063tXBtesswlw3g","authDomain":"el-plato-listo.firebaseapp.com","messagingSenderId":"672867578350"})), 
+    provideAuth(() => getAuth()), 
+    provideFirestore(() => getFirestore()),
   ],
 });
