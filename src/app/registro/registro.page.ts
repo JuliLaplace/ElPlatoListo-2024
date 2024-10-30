201a0ad25bd219e4df8df143d23488262e75dd0a
Laura Colque <lauraclq3@gmail.com>
2024-10-30 03:27:47 -0300

    [DISEÑO] Formulario de registro

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,20 +1,79 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IonContent, IonAvatar,IonHeader, IonTitle, IonToolbar, IonIcon, IonFabButton, IonFabList, IonCard, IonCardContent, IonNote, IonCardHeader, IonItem, IonButton, IonFab, IonInput } from '@ionic/angular/standalone';
+import { LoginService } from 'src/servicios/login.service';
+import { Router } from '@angular/router';
+import { SpinnerService } from 'src/servicios/spinner.service';
+import { FormViewerService } from 'src/servicios/form-viewer.service';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonNote, IonAvatar, IonFabButton, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ReactiveFormsModule, IonFabList, IonFabButton, IonCard, IonCardContent, IonCardHeader, IonItem, IonButton, IonFab, IonInput]
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  formRegistro!: FormGroup;
+  type: string = 'password';
+  isPassword!: boolean;
+  hide: boolean = false;
+  cargando: boolean = false;
 
-  ngOnInit() {
+  ngOnInit(): void {
+
+    if (this.type == 'password') this.isPassword = true;
+    this.formRegistro = new FormGroup({
+      nombre: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[a-zA-Z]+$'),
+      ]),
+      apellido: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[a-zA-Z]+$'),
+      ]),
+      dni: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[0-9]{8}$'),
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$'),
+      ]),
+      passwordLogin: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(10),
+      ]),
+      perfilImagen: new FormControl('', [Validators.required]),
+    });
   }
 
+  constructor(private loginService: LoginService, private spinnerService: SpinnerService, private formViewer: FormViewerService, private router: Router) {}
+
+
+  registrarse(){
+    this.formRegistro.reset();
+  }
+
+  controlError(control: string){
+    return this.formViewer.controlConError(this.formRegistro, control);
+  }
+
+  obtenerControlMensajeError(control: string){
+    return this.formViewer.mensajeError(this.formRegistro, control);
+  }
+
+  mostrarOcultarPassword() {
+    this.hide = !this.hide;
+    if (this.hide) this.type = 'password';
+    else this.type = 'text';
+  }
+
+  tomarFotoPefil(){
+
+  }
 }
