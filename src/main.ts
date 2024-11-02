91b0803bb810a68722c273bcd8c1ffd5803f7613
supercandela <supercandela@gmail.com>
2024-11-02 04:20:30 -0300

    [DEPLOY ANDROID] Build del proyecto para Android

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,15 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
+import { IonicModule } from '@ionic/angular';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)), 
+    importProvidersFrom(IonicModule.forRoot({animated: false})),
     provideFirebaseApp(() => initializeApp({"projectId":"el-plato-listo","appId":"1:672867578350:web:792bc52f8bd8002c619eff","storageBucket":"el-plato-listo.appspot.com","apiKey":"AIzaSyAWtH8J6rQU1LZ-8ixY063tXBtesswlw3g","authDomain":"el-plato-listo.firebaseapp.com","messagingSenderId":"672867578350"})), 
     provideAuth(() => getAuth()), 
     provideFirestore(() => getFirestore()),
