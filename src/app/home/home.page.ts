3595a100de1feead8f2acd294d83fe16a6d03d8f
Laura Colque <lauraclq3@gmail.com>
2024-11-18 00:25:35 -0300

    [HOME MOZO] - Se agregro componete del Mozo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,19 +8,25 @@ import { HomeAdminComponent } from '../componentes/home-admin/home-admin.compone
 import { TabsComponent } from "../componentes/tabs/tabs.component";
 import { HomeClienteComponent } from '../componentes/home-cliente/home-cliente.component';
 import { HomeMaitreComponent } from '../componentes/home-maitre/home-maitre.component';
+import { chatbubbleEllipsesOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+import { RouterLink } from '@angular/router';
+import { HomeMozoComponent } from '../componentes/home-mozo/home-mozo.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, HomeAdminComponent, TabsComponent, HomeClienteComponent, HomeMaitreComponent],
+  imports: [IonicModule, CommonModule, FormsModule, HomeAdminComponent, TabsComponent, HomeClienteComponent, HomeMaitreComponent, RouterLink, HomeMozoComponent],
 })
 export class HomePage {
 
   // usuario! : Usuario;
   constructor(public sesion: SesionService, public dataUsuario : DataUsuariosService) {
-
+     addIcons({
+       chatbubbleEllipsesOutline,
+     });
   }
 
 }
