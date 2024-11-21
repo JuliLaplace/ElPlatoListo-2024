7dafbe0189c70cb939c7a9404b8d0fd07c086241
JuliLaplace <juli.laplace@gmail.com>
2024-11-21 01:08:21 -0300

    [QR CLIENTE] Se agregó un botón adicional para escanear (en tab inferior)

--- src/app/componentes/tabs/tabs.component.ts
+++ src/app/componentes/tabs/tabs.component.ts
@@ -3,9 +3,11 @@ import { Component, Input, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { addIcons } from 'ionicons';
-import { home, menu, qrCodeOutline, exitSharp } from 'ionicons/icons';
+import { home, menu, qrCodeOutline, exitSharp, qrCode } from 'ionicons/icons';
 import { Usuario } from 'src/servicios/data-usuarios.service';
+import { GestionQrService } from 'src/servicios/gestion-qr.service';
 import { LoginService } from 'src/servicios/login.service';
+import { SesionService } from 'src/servicios/sesion.service';
 
 @Component({
   selector: 'app-tabs',
@@ -17,16 +19,22 @@ import { LoginService } from 'src/servicios/login.service';
 export class TabsComponent {
   @Input() usuario!: Usuario | null;
 
-  constructor(private loginService: LoginService) {
+  constructor(private loginService: LoginService, public sesion: SesionService, public qrServicio : GestionQrService) {
     addIcons({
       menu,
       home,
       qrCodeOutline,
       exitSharp,
+      qrCode
     });
   }
 
   cerrarSesion() {
     this.loginService.logout();
+  }
+
+
+  escanearQr(){
+    this.qrServicio.escanear();
   }
 }
